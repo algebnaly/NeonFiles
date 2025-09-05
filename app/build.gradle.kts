a6a5cab09404ee8ad2589d471d481d09e2c5eb6c
algebnaly <algebnaly@algebnaly.com>
2025-09-05 20:46:38 +0800

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,12 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
+
+val abiList = listOf("arm64-v8a", "x86_64")
 
 android {
     namespace = "com.algebnaly.neonfiles"
@@ -16,6 +20,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ndk {
+            //noinspection ChromeOsAbiSupport
+            abiFilters += abiList
+        }
+        ndkVersion = "27.3.13750724"
     }
 
     buildTypes {
@@ -31,8 +40,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
     }
     buildFeatures {
         compose = true
@@ -59,4 +70,27 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+
+val nfs4cLibProjectPath = "../../../rust/nfscrs_jni/"
+val jniStorePath = layout.projectDirectory.dir("src/main/jniLibs")
+val ndkPath = android.ndkDirectory.absolutePath
+
+
+tasks.named("preBuild") {
+    dependsOn(buildNFSCrsLib)
+}
+
+val buildNFSCrsLib by tasks.registering(Exec::class) {
+    inputs.dir("$nfs4cLibProjectPath/src")
+    outputs.dir(jniStorePath)
+    workingDir = File(nfs4cLibProjectPath)
+    commandLine = listOf(
+        "cargo", "ndk"
+    ) + abiList.flatMap { listOf("-t", it) } + listOf(
+        "-o", "$projectDir/src/main/jniLibs",
+        "build", "--release"
+    )
+    environment("ANDROID_NDK_HOME", ndkPath)
 }
